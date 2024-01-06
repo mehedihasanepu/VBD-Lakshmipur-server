2fdbb3dac525b11377ef6de0f926fe8df92dd6f9
mehedihasanepu <mehedihasanepu123@gmail.com>
2024-01-07 01:11:44 +0600

    complete post jwt and verifyToken and admin section. Also complete post and get users and currentUser

--- index.js
+++ index.js
@@ -29,11 +29,97 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
+        const userCollection = client.db("vbdLakshmipurDB").collection("users")
+
+
+        // jwt related API
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            })
+            res.send({ token })
+        })
 
 
 
 
 
+        //middleWare
+        const verifyToken = (req, res, next) => {
+            // console.log('Inside verify token:', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: ' unauthorized access' })
+                }
+                req.decoded = decoded;
+                next()
+            })
+        }
+
+
+
+        //use verifyAdmin after VerifyToken
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next()
+        }
+
+
+
+        // user related API 
+
+        app.get('/users', verifyToken, async (req, res) => {
+            const search = req.query.search;
+
+            const query = {};
+            if (search) {
+                query.name = { $regex: new RegExp(search, "i") };
+            }
+
+            const result = await userCollection
+                .aggregate([
+                    { $match: query }
+                ])
+                .toArray();
+            res.send(result);
+
+        })
+
+
+        app.get('/user/currentUser', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await userCollection.find(query).toArray();
+            res.send(result);
+
+        });
+
+
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
 
 
 
